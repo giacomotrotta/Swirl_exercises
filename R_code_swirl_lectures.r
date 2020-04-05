ac71eb7ed7fca4f00c44873f451be25aa66acc33
giacomotrotta <62661700+giacomotrotta@users.noreply.github.com>
2020-04-05 11:25:55 +0200

    Create R_code_swirl_lectures.r

---
+++ R_code_swirl_lectures.r
@@ -0,0 +1,8 @@
+#lectures 1 Basic Buildin Blocks
+
+z <- c(1.1, 9, 3.14) #creation of a vector.
+
+#vector can be used also fot operation like z*2+100 --> First, R multiplied each of the three elements in z by 2. Then it added 100 to each element to get the result you see above
+
+#Other common arithmetic operators are `+`, `-`, `/`, and `^` (where x^2 means 'x squared'). To take the square root, use the sqrt() function and
+#to take the absolute value, use the abs() function.
